35440727b7d8769219c99c12b39705c47881e84e
Adrisa <adrisansyarif@gmail.comgit config --global user.email adrisansyarif@gmail.com>
2024-03-31 10:26:48 +0700

    Add LinkedList

---
+++ Data Structure/3.LinkedList/LinkedList.js
@@ -0,0 +1,104 @@
+// let myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 10,
+//         next: null,
+//       },
+//     },
+//   },
+// };
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+
+    if (index === 0) {
+      this.prepend(value);
+      return this.printList();
+    }
+
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  remove(index) {
+    const leader = this.traverseToIndex;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+// console.log(myLinkedList);
+myLinkedList.append(5);
+// console.log(myLinkedList);
+myLinkedList.append(16);
+console.log(myLinkedList.printList());
+
+// console.log(myLinkedList);
+// console.log(myLinkedList.prepend(2));
+console.log(myLinkedList.insert(1, 101));
---
+++ Data Structure/3.LinkedList/ObjectPointer.js
@@ -0,0 +1,4 @@
+let obj1 = { a: true };
+let obj2 = obj1;
+obj2.a = "Wawan";
+console.log(obj1.a);
