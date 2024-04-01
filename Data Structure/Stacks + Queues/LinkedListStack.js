53e26e787ec4248e25cdba352e3ead1fbbc285f6
Adrisa <adrisansyarif@gmail.com>
2024-04-01 09:44:55 +0700

    Add placeholder Stacks + Queues

---
+++ Data Structure/Stacks + Queues/LinkedListStack.js
@@ -0,0 +1,35 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.top;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+    }
+  }
+
+  pop() {}
+
+  //isEmpty
+}
+
+const myStack = new Stack();
